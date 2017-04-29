f40f77dd4b104ed3f2ccb2e66cbdbed2ad5f8a94
jjpeleato <jjpeleato@gmail.com>
2017-04-29 23:42:56 +0200

    Event on map drag and get center lat/long

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,68 @@
+/**
+ * Global vars
+ */
+var url = '';
+var geo = {
+    'lat':'41.6563497',
+    'long':'-0.8765662'
+};
 var map = L.map('map').setView([41.6563497,  -0.8765662], 3);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+$(document).ready( function () {
+    initMap();
+});
 
-L.marker([41.6563497, -0.8765662]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+function initMap() {
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([geo['lat'], geo['long']]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+    onMapDrag();
+}
+
+function onMapDrag() {
+    map.on('dragend', function(event){
+        console.log(map.getCenter());
+    });
+};
+
+function getData(object, filter) {
+    var seed = '';
+    var uniqueKey = '';
+
+    $.ajax({
+        type: 'post',
+        url: $(this).attr('action'),
+        data: formData,
+        dataType: 'json',
+        cache: false,
+        contentType: false,
+        processData: false,
+        beforeSend: function () {
+            console.log('init');
+        },
+        success: function (data, textStatus, jqXHR) {
+            var result = data['data'];
+            if (result == 1){
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error(errorThrown);
+        },
+        xhr: function(){
+            var xhr = $.ajaxSettings.xhr();
+            xhr.upload.onprogress = function(evt){
+                var percent = parseInt(evt.loaded / evt.total * 100);
+                console.log(percent);
+            };
+            xhr.upload.onload = function(){
+                console.log('100%');
+            };
+            return xhr;
+        }
+    });
+}
